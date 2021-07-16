4f30b3c733685163ec58042f7bd615c290e4097c
Ivan <87426595+Psyke1989@users.noreply.github.com>
2021-07-16 13:44:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 
 import getpass
 
-
 location = input("Where you use that Login and Password: ")
 name = input("Input your login: ")
 password = getpass.getpass(prompt='Password: ', stream=None)
@@ -12,14 +11,7 @@ print(f"Check corretly or not I remeber your login "
       f"and password. Location: {location}, Login: {name}, Password:{password}")
 
 global agree
-agree = input("'yes' or 'no': ")
-
-if agree.lower() == "yes":
-    with open(f"{location}.txt", "w") as d:
-        print(f"Location: {location}", file=d)
-        print(f"Login: {name}", file=d)
-        print(f"Password: {password}", file=d)
-    print("All recorded")
+agree = str()
 
 while agree.lower() != "yes":
     agree = input("'yes' or 'no': ")
@@ -36,6 +28,6 @@ while agree.lower() != "yes":
 
         print(f"Check corretly or not I remeber your login "
               f"and password. Location: {location}, Login: {name}, Password:{password}")
-
     else:
         print("Type 'yes' or 'no' please. I not understand other words.")
+

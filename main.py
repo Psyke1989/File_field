ccb043fe681186193a4980087c82d6eb827acb70
Ivan <87426595+Psyke1989@users.noreply.github.com>
2021-07-16 08:50:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 
 import getpass
 
+
 location = input("Where you use that Login and Password: ")
 name = input("Input your login: ")
 password = getpass.getpass(prompt='Password: ', stream=None)
@@ -14,17 +15,16 @@ global agree
 agree = input("'yes' or 'no': ")
 
 if agree.lower() == "yes":
-    with open("l_and_p.txt", "w") as d:
+    with open(f"{location}.txt", "w") as d:
         print(f"Location: {location}", file=d)
         print(f"Login: {name}", file=d)
         print(f"Password: {password}", file=d)
     print("All recorded")
 
-
 while agree.lower() != "yes":
     agree = input("'yes' or 'no': ")
     if agree.lower() == "yes":
-        with open("l_and_p.txt", "w") as d:
+        with open(f"{location}.txt", "w") as d:
             print(f"Location: {location}", file=d)
             print(f"Login: {name}", file=d)
             print(f"Password: {password}", file=d)
@@ -33,7 +33,9 @@ while agree.lower() != "yes":
         location = input("Where you use that Login and Password: ")
         name = input("Input your login: ")
         password = getpass.getpass(prompt='Password: ', stream=None)
+
         print(f"Check corretly or not I remeber your login "
               f"and password. Location: {location}, Login: {name}, Password:{password}")
+
     else:
         print("Type 'yes' or 'no' please. I not understand other words.")
